56c6de27f9fe0852cea5a270e12a2a49dd6b0e5d
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-17 18:45:53 -0300

    Requesito 22 - retorna vooId conforme as condicoes

---
+++ challenges/desafio22.js
@@ -0,0 +1,11 @@
+db.voos.findOne(
+    { 
+      $or: [
+        { "empresa.nome": "DELTA AIRLINES" }, 
+        { "empresa.nome": "AMERICAN AIRLINES" },
+      ],
+      "aeroportoOrigem.sigla": "SBGR",
+      "aeroportoDestino.sigla": "KJFK",
+    },
+    { _id: false, vooId: 1 },
+);
